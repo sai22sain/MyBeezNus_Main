77a97445abb98c74fbbec19986c49767ca9c9477
SAI N <143072125+mrAnonymous002@users.noreply.github.com>
2026-09-11 21:52:30 +0530

    chore: remove firebase tree, allow reviewed postinstall scripts, silence npm notices

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="527186193343-rd67ng6fqag1ifikc1c0tvndv1uli7u8.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
